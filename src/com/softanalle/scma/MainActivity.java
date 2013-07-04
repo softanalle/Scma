3375a09f5c990eeeb0487b4862e4389b1e70cb9c
Tommi Rintala <tommi.rintala@gmail.com>
2013-07-04 22:38:33 +0300

    Add ledindicator control to application

--- src/com/softanalle/scma/MainActivity.java
+++ src/com/softanalle/scma/MainActivity.java
@@ -11,9 +11,9 @@ import android.widget.ToggleButton;
 
 public class MainActivity extends Activity {
 
-    private static String APPTAG = "SCMA0_1";
+	private static String APPTAG = "SCMA0_1";
 
-	private ToggleButton button1_, button2_, button3_;
+	private ToggleButton button1_, button2_, button3_, onOffBtn_;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -21,6 +21,7 @@ public class MainActivity extends Activity {
 		button1_ = (ToggleButton) findViewById(R.id.toggleButton1);
 		button2_ = (ToggleButton) findViewById(R.id.toggleButton2);
 		button3_ = (ToggleButton) findViewById(R.id.toggleButton3);
+		onOffBtn_ = (ToggleButton) findViewById(R.id.boardOnOffButton);
 
 		button1_.setOnClickListener(new OnClickListener() {
 
@@ -48,15 +49,34 @@ public class MainActivity extends Activity {
 			}
 
 		});
+
+		onOffBtn_.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				extOnOff(((ToggleButton) v).isChecked());
+			}
+		});
 	}
 
+	@SuppressWarnings("unused")
+	private boolean boardOnOff_ = false;
 
+	/*
+	 * to be called, when user wishes to turn board on/off
+	 */
+	private void extOnOff(boolean checked) {
+		// TODO Auto-generated method stub
+		Log.d(APPTAG, "Turn power of ext: " + checked);
+		boardOnOff_ = checked;
+	}
 
 
 	private void donClick(View v) {
 		// TODO Auto-generated method stub
-        Log.d(APPTAG, "Clicked: " + v);
-        Log.d(APPTAG, "  => " + ((ToggleButton) v).isChecked());
+		Log.d(APPTAG, "Clicked: " + v);
+		Log.d(APPTAG, "  => " + ((ToggleButton) v).isChecked());
 
 	}
 
