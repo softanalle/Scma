98cc3889fa84e321807cd114836d8a3fdab21151
Tommi Rintala <tommi.rintala@gmail.com>
2013-07-18 21:58:54 +0300

    Pre-beta of scm

--- ScmCtrls/src/com/softanalle/scmctrls/LedArrayIndicator.java
+++ ScmCtrls/src/com/softanalle/scmctrls/LedArrayIndicator.java
@@ -1,9 +1,5 @@
 package com.softanalle.scmctrls;
 
-/**
- *  @author Tommi Rintala <tommi.rintala@iki.fi>
- */
-
 import android.view.View;
 import android.content.Context;
 import android.graphics.Canvas;
@@ -15,10 +11,12 @@ import android.util.AttributeSet;
 import android.util.Log;
 // import android.widget.TextView;
 
-public class LedIndicator extends View {
+public class LedArrayIndicator extends View {
 	private boolean isInitialized = false;
-	private static String TAG = "LedIndicator";
-	private int[] mRectColors = {
+	private static String TAG = "LedArrayIndicator";
+	private int[] mRectColors;
+	/*
+	= {
 			Color.WHITE,
 			Color.RED,
 			Color.GREEN,
@@ -28,6 +26,8 @@ public class LedIndicator extends View {
 			Color.YELLOW,
 			Color.CYAN
 	};
+	*/
+	/*
 	public static final int LED_WHITE = 0;
 	public static final int LED_RED = 1;
 	public static final int LED_GREEN = 2;
@@ -37,38 +37,48 @@ public class LedIndicator extends View {
 
 	protected static final int LED_FOCUS = 6;
 	protected static final int LED_CALIBRATE = 7;
-	
-	protected static final int LED_POWER = 64;
 
+	*/
 	private int mOffColor = Color.LTGRAY;
-	private int POWER_COLOR = Color.GREEN;
-	
-	private final static int mLedCount = 8;
 
-	public LedIndicator(Context context) {
+	private static int mLedCount = 0;
+
+	public LedArrayIndicator(Context context) {
 		super(context);
 		// TODO Auto-generated constructor stub
 		if (!isInitialized) initializeFirstTime();
 	}
-	public LedIndicator(Context context, AttributeSet attrs) {
+	public LedArrayIndicator(Context context, AttributeSet attrs) {
 		super(context, attrs);
 		if (!isInitialized) initializeFirstTime();
 	}
 
-	public LedIndicator(Context context, AttributeSet attrs, int defStyle) {
+	public LedArrayIndicator(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
 		if (!isInitialized) initializeFirstTime();
 	}
 
+	
+	
+	public boolean setConfig(int colors[]) {
+		if ( colors.length > 0) {
+		mLedCount = colors.length;
+		mRectColors = new int[mLedCount];
+		for (int i=0;i<colors.length;i++) {
+			
+		}
+		} else {
+			// not allowed configuration
+			return false;
+		}
+		return true;
+	}
 
 	private Rect[] mLedArea;
-	private Rect mPowerArea;
 	private ShapeDrawable[] mLeds;
-	private ShapeDrawable mPowerLed;
 	private boolean[] mActivity;
 	private int mLeft = 0;
 	private int mTop = 0;
-	private int mRight = 0;
 	private int mHeight = 20;
 	private int mWidth = 24;
 	private boolean isReady;
@@ -95,13 +105,8 @@ public class LedIndicator extends View {
 				int x = mLeft + 2 + i * mWidth;
 				int y = mTop + 2 ;
 				mLeds[i].setBounds(x, y, x + mWidth, y + mHeight);
-				mLedArea[i] = new Rect( mRight - mWidth, y, mRight, y + mHeight );
+				mLedArea[i] = new Rect(x, y, x + mWidth, y+mHeight);
 			}
-			mPowerLed = new ShapeDrawable(new RectShape());
-			mPowerLed.getPaint().setColor(mOffColor);
-			mPowerArea = new Rect( mRight - mWidth, mTop + 2, mWidth, mTop + 2 + mHeight );
-			
-			mPowerLed.setBounds(mPowerArea);
 		} catch (Exception ex) {
 			Log.e(TAG, "Error while building leds");
 		} finally {
@@ -110,67 +115,41 @@ public class LedIndicator extends View {
 	}
 
 	@Override
-	public void onLayout(boolean changed, int left, int top, int right, int bottom) {
-		Log.d(TAG, "onLayout(" + changed + "," + left + "," + top + "," + right + "," + bottom + ")");          
+	public void onLayout(boolean changed, int left, int top, int rigth, int bottom) {
+		Log.d(TAG, "onLayout(" + changed + "," + left + "," + top + "," + rigth + "," + bottom + ")");          
 		if (!isInitialized) initializeFirstTime();      
 
 		if ( changed ) {
 			mHeight = (int) (bottom - top) - 4;
 			mWidth = mHeight;
-			mRight = right;
 			for (int i=0;i<mLedCount;i++) {
 				int x = mLeft + i * 2 + i * mWidth;
 				int y = mTop + 2;
 				Log.d(TAG, "setPos:" + x + ", " + y + ", " + mWidth + ", " + mHeight);
 				mLedArea[i].left = x;
 				mLedArea[i].top = y;
-				mLedArea[i].bottom = y + mHeight;
-				mLedArea[i].right = x + mWidth;                           
+				mLedArea[i].bottom = y+mHeight;
+				mLedArea[i].right = x+mWidth;                           
 				if (mLeds[i] != null)
 					mLeds[i].setBounds(mLedArea[i]);
 			}
-			mPowerArea.left = mRight - mWidth;
-			mPowerArea.right = mRight;
-			mPowerArea.top = mTop + 2;
-			mPowerArea.bottom = mTop + 2 + mHeight;
-			mPowerLed.setBounds(mPowerArea);
 		}
 	}
 
 	@Override
 	public void onDraw(Canvas canvas) {
-		if (isReady && canvas != null) {
+		if (isReady) {
 			for (int i=0;i<mLedCount;i++) {
-				if(mLeds[i] != null )
+				if(mLeds[i] != null && canvas != null)
 					mLeds[i].draw(canvas);
-			}
-			if ( mPowerLed != null) {
-				mPowerLed.draw(canvas);
 			}
 		}
 		invalidate();
 	}
 
-	public void setPowerState(boolean on) {
-		if ( on ) {
-			mPowerLed.getPaint().setColor(POWER_COLOR);
-		} else {
-			mPowerLed.getPaint().setColor(mOffColor);	
-		}
-		invalidate(mPowerArea);
-	}
-	
-	/**
-	 * @method turnOn index
-	 * turn on led indicator location index
-	 */
-	
-	/**
-	 * @param index 
-	 */
-	public void turnOn(int index) {		
+	public void turnOn(int index) {
+		Log.d(TAG, "turnOn(" + index + ")");
 		if (index >= 0 && index < mLedCount) {
-			Log.d(TAG, "turnOn(" + index + ")");
 			mActivity[index] = true;
 			mLeds[index].getPaint().setColor(mRectColors[index]);
 		}
@@ -178,12 +157,9 @@ public class LedIndicator extends View {
 		invalidate(mLedArea[index]);
 	}
 
-	/**
-	 * @param index
-	 */
-	public void turnOff(int index) {		
+	public void turnOff(int index) {
+		Log.d(TAG, "turnOff(" + index + ")");
 		if ( index >= 0 && index < mLedCount) {
-			Log.d(TAG, "turnOff(" + index + ")");
 			mActivity[index] = false;
 			mLeds[index].getPaint().setColor(mOffColor);
 		}
---
+++ ScmTest1/src/com/softanalle/scmtest1/ScmTest1.java
@@ -0,0 +1,337 @@
+package com.softanalle.scmtest1;
+
+/**
+ * @author Tommi Rintala <t2r@iki.fi>
+ */
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import ioio.lib.api.AnalogInput;
+import ioio.lib.api.DigitalOutput;
+import ioio.lib.api.IOIO;
+import ioio.lib.api.PwmOutput;
+import ioio.lib.api.exception.ConnectionLostException;
+import ioio.lib.util.BaseIOIOLooper;
+import ioio.lib.util.IOIOLooper;
+import ioio.lib.util.android.IOIOActivity;
+import android.app.admin.DevicePolicyManager;
+import android.hardware.Camera;
+import android.hardware.Camera.PictureCallback;
+import android.hardware.Camera.ShutterCallback;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+
+import com.softanalle.scmctrls.LedIndicator;
+//import com.softanalle.previewtest.Preview;
+import com.softanalle.scmtest1.R;
+
+
+// camera stuff
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import android.hardware.Camera;
+import android.hardware.Camera.PictureCallback;
+import android.hardware.Camera.ShutterCallback;
+import android.os.Bundle;
+import android.os.Environment;
+import android.app.Activity;
+import android.content.Context;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.FrameLayout;
+
+import android.app.admin.DevicePolicyManager;
+
+public class ScmTest1 extends IOIOActivity {
+
+	private ToggleButton toggleButton_;
+	private LedIndicator ledIndicator_;
+	private Button pictureButton_;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+
+		pictureButton_  = (Button) findViewById(R.id.pictureButton);
+		toggleButton_ = (ToggleButton) findViewById(R.id.ToggleButton);
+
+		ledIndicator_ = (LedIndicator) findViewById(R.id.ledIndicator1);
+
+		// camera stuff
+		mPreview = new Preview(this);
+		FrameLayout tmp = (FrameLayout) findViewById(R.id.RootFrame);
+		tmp.addView(mPreview);
+
+		ledIndicator_.bringToFront();
+		toggleButton_.bringToFront();
+		pictureButton_.bringToFront();
+
+		//buttonClick = (Button) findViewById(R.id.button1);
+
+		pictureButton_.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				if ( powerState_ && mPreview.isReady() ) {
+					powerLedsOn();
+					mPreview.camera.takePicture(shutterCallback, rawCallback, jpegCallback);
+					powerLedsOff();
+				}
+			}
+		});
+
+
+		toggleButton_.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				// mPreview.camera.takePicture(shutterCallback, rawCallback, jpegCallback);
+				if ( ((ToggleButton) v).isChecked()) {
+					powerState_ = true;
+					pictureButton_.setEnabled(true);
+				} else {
+					powerState_ = false;
+					pictureButton_.setEnabled(false);
+				}						
+			}
+		});
+
+		pictureButton_.setEnabled(false);
+		
+		Log.d(TAG, "onCreate'd");
+
+		mDPM = (DevicePolicyManager) getSystemService(Context.DEVICE_POLICY_SERVICE);
+		Log.d(TAG, "  getCameraDisabled(): " + mDPM.getCameraDisabled(null));
+
+
+		enableUi(false);
+	}
+
+	private boolean powerState_ = false;
+	
+	private void powerLedsOn() {
+		pulseWidth1_ = 1000;
+		pulseWidth2_ = 1000;
+		pulseWidth3_ = 1000;
+		
+		ledIndicator_.turnOn(0);
+		ledIndicator_.turnOn(1);
+		ledIndicator_.turnOn(2);
+	}
+
+	private void powerLedsOff() {
+		pulseWidth1_ = 0;
+		pulseWidth2_ = 0;
+		pulseWidth3_ = 0;
+		
+		ledIndicator_.turnOff(0);
+		ledIndicator_.turnOff(1);
+		ledIndicator_.turnOff(2);		
+	}
+
+	private int pulseWidth1_, pulseWidth2_, pulseWidth3_;
+
+	class Looper extends BaseIOIOLooper {
+		// private AnalogInput input_;
+		private PwmOutput pwmOutput1_;
+		private PwmOutput pwmOutput2_;
+		private PwmOutput pwmOutput3_;
+		private DigitalOutput led_;
+		private DigitalOutput powout_;
+
+		@Override
+		public void setup() throws ConnectionLostException {
+			led_ = ioio_.openDigitalOutput(IOIO.LED_PIN, true);
+			powout_ = ioio_.openDigitalOutput(6, true);
+			//input_ = ioio_.openAnalogInput(40);
+
+			pwmOutput1_ = ioio_.openPwmOutput(10, 100);
+			pwmOutput2_ = ioio_.openPwmOutput(11, 100);
+			pwmOutput3_ = ioio_.openPwmOutput(12, 100);
+
+			pwmOutput1_.setPulseWidth( 0 );
+			pwmOutput2_.setPulseWidth( 0 );
+			pwmOutput3_.setPulseWidth( 0 );
+
+			powout_.write( false );
+
+			try {
+				File saveDir = new File(String.format("%s/SCM", Environment.getExternalStorageDirectory().getPath()));
+				if ( ! saveDir.isDirectory()) {
+					saveDir.mkdir();
+				}
+			} catch (Exception e) {
+				Log.d(TAG, "Error in creation of savedir");
+			}
+			enableUi(true);
+		}
+
+		@Override
+		public void loop() throws ConnectionLostException, InterruptedException {
+
+			visualize( pulseWidth1_, pulseWidth2_, pulseWidth3_ );
+			
+			//setNumber( 1, pulseWidth1_ );
+			//setNumber( 2, pulseWidth2_ );
+			//setNumber( 3, pulseWidth3_ );
+			
+			pwmOutput1_.setPulseWidth( pulseWidth1_ );
+			pwmOutput2_.setPulseWidth( pulseWidth2_ );
+			pwmOutput3_.setPulseWidth( pulseWidth3_ );
+
+			led_.write( !powerState_ );
+			/*
+			 * powout_.write(toggleButton_.isChecked());
+			 */
+			//ledIndicator_.setPowerState( powerState_ );
+			Thread.sleep(10);
+		}
+
+		@Override
+		public void disconnected() {
+			enableUi(false);
+		}
+	}
+
+	@Override
+	protected IOIOLooper createIOIOLooper() {
+		return new Looper();
+	}
+
+	private void enableUi(final boolean enable) {
+		runOnUiThread(new Runnable() {
+			@Override
+			public void run() {
+				/*
+				seekBar1_.setEnabled(enable);
+				seekBar2_.setEnabled(enable);
+				seekBar3_.setEnabled(enable);
+				 */
+
+				//toggleButton_.setEnabled(enable);
+				//mPreview.setEnabled(enable);
+
+			}
+		});
+	}
+
+	private void setNumber(int channel, float f) {
+		final String str = String.format("%.2f", f);
+		final int ch = channel;
+		runOnUiThread(new Runnable() {
+			@Override
+			public void run() {
+				/*
+				switch(ch) {
+				case 1:
+					textView1_.setText(str);
+					break;
+				case 2:
+					textView2_.setText(str);
+					break;
+				case 3:
+					textView3_.setText(str);
+					break;
+				default:
+					// no action
+				}
+				 */
+				ledIndicator_.setPowerState( powerState_ );				
+			}
+		});
+	}
+	
+	private void visualize(int pw1, int pw2, int pw3) {
+		runOnUiThread(new Runnable() {
+			@Override
+			public void run() {
+				ledIndicator_.setPowerState(powerState_);
+			}
+		});
+	}
+
+	// --------------------------------------------------------------------------------
+	// camera stuff
+
+
+
+	private static final String TAG = "ScmTest1";
+	Camera mCamera;
+	Preview mPreview;
+	Button buttonClick;
+
+	DevicePolicyManager mDPM;
+
+
+	ShutterCallback shutterCallback = new ShutterCallback() {
+		public void onShutter() {
+			Log.d(TAG, "onShutter");
+		}
+	};
+
+	PictureCallback rawCallback = new PictureCallback() {
+		public void onPictureTaken(byte[] data, Camera camera) {
+			Log.d(TAG, "onPictureTaken() - raw");
+			/*
+			FileOutputStream outStream = null;
+			try {
+				outStream = new FileOutputStream(String.format("%s/SCM/%d.raw",
+						Environment.getExternalStorageDirectory().getPath(),
+						System.currentTimeMillis()));
+				outStream.write(data);
+				outStream.close();
+				Log.d(TAG, "onRawPictureTaken - wrote bytes: " + data.length);
+				Toast.makeText(getApplicationContext(), "RAW - wrote bytes: " + data.length, Toast.LENGTH_LONG).show();
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			} finally {
+			}
+			Log.d(TAG, "onPictureTaken - raw");
+			*/
+		}
+	};
+
+	PictureCallback jpegCallback = new PictureCallback() {
+		public void onPictureTaken(byte[] data, Camera camera) {
+			FileOutputStream outStream = null;
+			try {
+				outStream = new FileOutputStream(String.format("%s/SCM/%d.jpg",
+						Environment.getExternalStorageDirectory().getPath(),
+						System.currentTimeMillis()));
+				outStream.write(data);
+				outStream.close();
+				Log.d(TAG, "onPictureTaken - wrote bytes: " + data.length);
+				Toast.makeText(getApplicationContext(), "JPEG - wrote bytes: " + data.length, Toast.LENGTH_LONG).show();
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			} finally {
+			}
+			Log.d(TAG, "onPictureTaken - jpeg");
+		}
+	};
+
+
+}
