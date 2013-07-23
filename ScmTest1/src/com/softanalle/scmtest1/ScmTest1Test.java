c9a116ba1b807adb4614262bbc28c820d81e494a
Tommi Rintala <tommi.rintala@gmail.com>
2013-07-23 22:23:25 +0300

    JUnit test module added

---
+++ ScmTest1/src/com/softanalle/scmtest1/ScmTest1Test.java
@@ -0,0 +1,43 @@
+package com.softanalle.scmtest1;
+
+import junit.framework.TestCase;
+
+public class ScmTest1Test extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	public void testOnResume() {
+		fail("Not yet implemented");
+	}
+
+	public void testOnPause() {
+		fail("Not yet implemented");
+	}
+
+	public void testOnCreateBundle() {
+		fail("Not yet implemented");
+	}
+
+	public void testCreateIOIOLooper() {
+		fail("Not yet implemented");
+	}
+
+	public void testOnCreateOptionsMenuMenu() {
+		fail("Not yet implemented");
+	}
+
+	public void testOnOptionsItemSelectedMenuItem() {
+		fail("Not yet implemented");
+	}
+
+	public void testOnSharedPreferenceChanged() {
+		fail("Not yet implemented");
+	}
+
+}
