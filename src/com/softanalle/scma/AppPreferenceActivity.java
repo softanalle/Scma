6126850f6447a3e3e849d6664fb73a79c33861ec
Tommi Rintala <tommi.rintala@gmail.com>
2013-08-20 10:27:42 +0300

    Closes #18

--- src/com/softanalle/scma/AppPreferenceActivity.java
+++ src/com/softanalle/scma/AppPreferenceActivity.java
@@ -3,8 +3,11 @@ package com.softanalle.scma;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
+import android.preference.EditTextPreference;
+import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceCategory;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
@@ -46,7 +49,10 @@ implements OnSharedPreferenceChangeListener
 		setTheme(android.R.style.Theme_Black_NoTitleBar_Fullscreen);
 		super.onCreate(savedInstanceState);        
 		
-		addPreferencesFromResource(R.xml.preferences);        
+		addPreferencesFromResource(R.xml.preferences);  
+		for (int i = 0; i < getPreferenceScreen().getPreferenceCount(); i++) {
+            initSummary(getPreferenceScreen().getPreference(i));
+        }
 	}
 
 	@Override
@@ -72,12 +78,55 @@ implements OnSharedPreferenceChangeListener
 
 
 	//@SuppressWarnings("deprecation")
+	@SuppressWarnings("deprecation")
 	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+		/*
         if (key.equals(MainActivity.KEY_PREF_FOCUSCOLOR)) {
             Preference connectionPref = findPreference(key);
             // Set summary to be the user-description for the selected value
             connectionPref.setSummary(sharedPreferences.getString(key, "Selected color"));
         }
+        */
+        updatePrefSummary(findPreference(key));
     }
 
+	@SuppressWarnings("deprecation")
+	@Override
+    protected void onResume() {
+        super.onResume();
+        // Set up a listener whenever a key changes
+        getPreferenceScreen().getSharedPreferences()
+                .registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @SuppressWarnings("deprecation")
+	@Override
+    protected void onPause() {
+        super.onPause();
+        // Unregister the listener whenever a key changes
+        getPreferenceScreen().getSharedPreferences()
+                .unregisterOnSharedPreferenceChangeListener(this);
+    }
+    
+    private void initSummary(Preference p) {
+        if (p instanceof PreferenceCategory) {
+            PreferenceCategory pCat = (PreferenceCategory) p;
+            for (int i = 0; i < pCat.getPreferenceCount(); i++) {
+                initSummary(pCat.getPreference(i));
+            }
+        } else {
+            updatePrefSummary(p);
+        }
+    }
+
+    private void updatePrefSummary(Preference p) {
+        if (p instanceof ListPreference) {
+            ListPreference listPref = (ListPreference) p;
+            p.setSummary("Currently: " + listPref.getEntry());
+        }
+        if (p instanceof EditTextPreference) {
+            EditTextPreference editTextPref = (EditTextPreference) p;
+            p.setSummary("Current: " + editTextPref.getText());
+        }
+    }
 }
